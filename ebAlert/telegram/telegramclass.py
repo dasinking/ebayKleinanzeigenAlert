70ab823da0c6f859e71e60054adba28e2b4237b3
Alexander <alexander@dasinking.de>
2024-10-29 01:58:41 +0100

    patch telegramclass.py for use with ntfy

--- ebAlert/telegram/telegramclass.py
+++ ebAlert/telegram/telegramclass.py
@@ -6,25 +6,32 @@ from urllib.parse import urlencode
 
 
 def send_message(message):
-    send_text_url = settings.TELEGRAM_API_URL + message + ""
-    response = requests.get(send_text_url)
-    return response.json()['ok']
+    ntfy_url = settings.NTFY_URL
+    headers = {
+            "Authorization": f"Bearer {settings.NTFY_TOKEN}"
+        }
+    response = requests.post(ntfy_url, data=message.encode("utf-8"), headers=headers)
+    return response.status_code == 200 
+
 
 
 class SendingClass:
 
     def send_message(self, message):
-        message_encoded = urlencode({"text": message})
-        sending_url = settings.TELEGRAM_API_URL + message_encoded + ""
-        response = requests.get(sending_url)
+        ntfy_url = settings.NTFY_URL
+        headers = {
+            "Authorization": f"Bearer {settings.NTFY_TOKEN}"
+        }
+        response = requests.post(ntfy_url, data=message.encode("utf-8"), headers=headers)
 
-        if response == 200:
-            return response.json()["ok"]
+        if response.status_code == 200:
+            return True
+        return False
 
     def send_formated_message(self, item: EbayItem):
-        message = f"{item.title}\n\n{item.price} ({item.city})\n\n"
-        url = f'<a href="{item.link}">{item.link}</a>'
-        self.send_message(message + url)
+        message = f"{item.title}\n\n{item.price} ({item.city})\n\n {item.description} \n\n {item.link}"
+        self.send_message(message)
 
 
 telegram = SendingClass()
+
