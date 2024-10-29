2a28764735aff72cc7f4a6c6b119bb2203469920
Alexander <alexander@dasinking.de>
2024-10-29 01:59:45 +0100

    patch config.py for ntfy

--- ebAlert/core/config.py
+++ ebAlert/core/config.py
@@ -3,12 +3,11 @@ import logging
 
 
 class Settings:
-    TOKEN = os.environ.get("TOKEN") or "Your_secret_key"
-    CHAT_ID = os.environ.get("CHAT_ID") or "Your_chat_id"
     FILE_LOCATION = os.path.join(os.path.expanduser("~"), "ebayklein.db")
-    TELEGRAM_API_URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&parse_mode=HTML&"""
     LOGGING = os.environ.get("LOGGING") or logging.ERROR
     URL_BASE = "https://www.kleinanzeigen.de"
+    NTFY_URL = "SERVER URL HERE"
+    NTFY_TOKEN = "tk_xxxxxxxxxxxxxxxxx"
 
 
 settings = Settings()
